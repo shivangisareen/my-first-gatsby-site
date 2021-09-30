fdeb05c5e0e611a908473c9be61ece2b8239609a
Shivangi Sareen <sareens@tcd.ie>
2021-10-01 00:07:10 +0100

    add 'about' page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p> This is where you can find more about us </p>
+        </div>
+    );
+};
+
+export default AboutPage;
