3bd879b4a6d0ad3ea57e6f98529008b6f173ef84
Shivangi Sareen <sareens@tcd.ie>
2021-10-02 22:53:07 +0100

    install plugin and dependencies to add images; add static image to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import {StaticImage} from "gatsby-plugin-image";
 
 import Layout from "../components/layout";
 
@@ -6,6 +7,10 @@ const IndexPage = () => {
     return (
       <div>
           <Layout subTitle="Home" subText="Welcome to the home page of my website!"/>
+          <StaticImage 
+          src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+          alt="Cute dog"
+          />
       </div>
     );
 };
