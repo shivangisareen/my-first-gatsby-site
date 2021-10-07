ce38fd90a88b4ae5d2a8b8577f6ae7f4b83ae811
Shivangi Sareen <sareens@tcd.ie>
2021-10-07 11:54:40 +0100

    use file system route api for blog posts

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import Layout from "../../components/layout";
+
+
+const BlogPost = () => {
+    return (
+        <div>
+            <Layout subTitle="Blog Posts Here!">
+                <p>My blog posts will be posted here!</p>
+            </Layout>
+        </div>
+    )
+};
+
+export default BlogPost;
+
