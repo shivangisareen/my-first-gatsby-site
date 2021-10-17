cd05da04bf886ba5b69f8cdc4bdeabfd1e6b0cae
Shivangi Sareen <sareens@tcd.ie>
2021-10-17 18:04:54 +0100

    link images to respective blog posts

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,14 +1,22 @@
 import * as React from "react";
 import { graphql } from 'gatsby';
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 import Layout from "../../components/layout";
 
 
 const BlogPost = ({data}) => {
+    const image = getImage(data.mdx.frontmatter.hero_image);
+    console.log(data);
     return (
         <div>
-            <Layout subTitle="Blog Posts Here!">
+            <Layout subTitle={data.mdx.frontmatter.title}>
+                <p>Date posted: {data.mdx.frontmatter.date}</p>
+                <GatsbyImage
+                    image = {image}
+                    alt={data.mdx.frontmatter.hero_image_alt}
+                />
                 <MDXRenderer>
                     {data.mdx.body}
                 </MDXRenderer>
@@ -20,16 +28,22 @@ const BlogPost = ({data}) => {
 export const query = graphql`
     query ($id: String) {
         mdx(id: {eq: $id}) {
-        frontmatter {
-            date
-            title
-        }
+            frontmatter {
+                date(formatString: "DD-MM-YYYY")
+                title
+                hero_image_alt
+                hero_image_credit_link
+                hero_image_credit_text
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
+            }
         body
         }
     }
 `
-
-
 
 export default BlogPost;
 
